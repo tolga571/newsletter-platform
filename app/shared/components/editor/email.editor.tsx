25e52a2db9156ddee3b249bcd8aa0f1e667a0d01
tolga571 <fsm1453857@gmail.com>
2024-10-23 15:57:21 +0300

    email editor edded 2

--- app/shared/components/editor/email.editor.tsx
+++ app/shared/components/editor/email.editor.tsx
@@ -4,15 +4,42 @@ import React, { useEffect, useRef, useState } from "react";
 import { DefaultJsonData } from "@/app/assets/mails/default";
 import { useClerk } from "@clerk/nextjs";
 
-const Emaileditor = ({subjectTitle}:{subjectTitle:string}) => {
-    const [loading, setLoading] = useState(true);
-    const [jsonData, setJsonData] = useState<any | null>(DefaultJsonData);
-    const {user} = useClerk();
-    const emailEditorRef = useRef<EditorRef>(null);
+const Emaileditor = ({ subjectTitle }: { subjectTitle: string }) => {
+  const [loading, setLoading] = useState(false);
+  const [jsonData, setJsonData] = useState<any | null>(DefaultJsonData);
+  const { user } = useClerk();
+  const emailEditorRef = useRef<EditorRef>(null);
+
+  const exportHtml = () => {
+    const unlayer = emailEditorRef.current?.editor;
+
+    unlayer?.exportHtml((data) => {
+      const { design, html } = data;
+      setJsonData(design);
+    });
+  };
+
+  const onReady: EmailEditorProps["onReady"] = () => {
+    const unlayer: any = emailEditorRef.current?.editor;
+    unlayer.loadDesign(jsonData);
+  };
 
   return (
-    <div>Emaileditor</div>
-  )
-}
+    <>
+      {!loading && (
+        <div className="w-full h-[90vh] relative">
+          <EmailEditor
+            minHeight={"80vh"}
+            ref={emailEditorRef}
+            onReady={onReady}
+          />
+          <div className="absolute bottom-0 flex items-center justify-end gap-4 right-0 w-full border-t p-3">
 
-export default Emaileditor
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Emaileditor;
